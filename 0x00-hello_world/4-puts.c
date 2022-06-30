35cba0e5f025e5de1c79d819eda7e49b6894c29c
Fana Abera <fanaabera@gmail.com>
2022-06-30 15:04:34 -0700

    Task 4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - entry poing
+ *
+ *Return: # after pringing of the function
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle\n");
+	return (0);
+}
