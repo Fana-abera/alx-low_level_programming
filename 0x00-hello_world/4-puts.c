4c36b221d56c53ffc114fa258717d76f3b3f973d
Fana Abera <fanaabera@gmail.com>
2022-06-30 15:52:05 -0700

    Task 4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *main - entry poing
+ * main - Entry point
  *
- *Return: # after pringing of the function
- */
-int main(void)
+ * Return: Always 0 (sucess)
+  */
+  int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle\n");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle\n");
+ return (0);
 }
