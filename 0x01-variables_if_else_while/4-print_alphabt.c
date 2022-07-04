99be2dfd82112624f40e98f9f3d320b1f28e7778
Fana Abera <fanaabera@gmail.com>
2022-07-04 09:11:22 -0700

    Task 4. When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,16 +12,19 @@ int main(void)
 
 	while (letter <= 'z')
 	{
-		putchar(letter);
-		letter++;
-	}
-
-	letter = 'A';
-
-	while (letter <= 'Z')
-	{
-		putchar(letter);
-		letter++;
+		if (letter == 'e')
+		{
+			letter++;
+		}
+		else if (letter == 'q')
+		{
+			letter++;
+		}
+		else
+		{
+			putchar(letter);
+			letter++;
+		}
 	}
 	putchar('\n');
 	return (0);
