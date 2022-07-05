d02dec957bd0191af9ba4e7c451732cfec616e92
Fana Abera <fanaabera@gmail.com>
2022-07-05 12:33:22 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
 *main - print the first 50 fibonacci numbers
 *Description: prints the first 50 Fibonacci numbers, starting with 1 and 2
@@ -9,12 +8,9 @@
 int main(void)
 {
 	int count;
-
 	unsigned long x, y, z;	
 	x = 0;
-
 	y = 1;
-
 	for (count = 1; count <= 49; count++)
 	{
 		z = x + y;
