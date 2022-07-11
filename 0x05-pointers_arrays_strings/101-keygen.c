8b1f57c28494bda0eaf635bea1ac5c0e6b966b45
Fana Abera <fanaabera@gmail.com>
2022-07-11 13:37:56 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,27 +1,32 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "main.h"
 
 /**
- * main - generate password
- *
- * Return: Always 0
- */
-int main(void)
-{
-	int r = 0;
-	int s = 0;
-	time_t t;
+*generationRandomPassword - Randomly generate passwords.
+*@N: password length.
+*Decription: Function to randomly generates password.
+*Return: nothing
+*/
 
-	srand((unsigned int) time(&t));
-	while (s < 2772)
-	{
-		r = rand() % 128;
-		if ((s + r) > 2772)
-			break;
-		s = s + r;
-		printf("%c", r);
-	}
-	printf("%c\n", (2772 - s));
+int main()
+{
+	generationRandomPassword(15);
 	return (0);
+}
+
+void generationRandomPassword(int N)
+{
+	char list[] = "1234567890qwertyuiopasdfghjklzxcvbnm!@#$%^&*()_- +=QWERTYUIOPASDFGHJKLZXCVBNM[]{};':\"<>,.?/|";
+
+	int i;
+	
+	srand(time(NULL));
+
+	for (i = 0; i < N; i++)
+	{
+		printf("%c", list[rand() % (sizeof(list) - 1)]);
+	}
 }
