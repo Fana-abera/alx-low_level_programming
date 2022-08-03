bb126aa0807ec8759a5c2a44db727c8cc37a4517
Fana Abera <fanaabera@gmail.com>
2022-08-03 12:17:23 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,38 +1,44 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * main - check the code
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: Always 0
- */
+ * main - function that Prints result of an operation
+ * @argc: Number of arguments
+ * @argv: Value of arguments
+ * Return: Returns an int.
+**/
+
 int main(int argc, char *argv[])
 {
-	int i, j;
-	int (*operation)(int, int);
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	if (argv[2][1])
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	operation = get_op_func(argv[2]);
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
 
-	if (operation == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	
-	i = atoi(argv[1]);
-	j = atoi(argv[3]);
+if (
+(strcmp(argv[2], "+") &&
+strcmp(argv[2], "-") &&
+strcmp(argv[2], "*") &&
+strcmp(argv[2], "/") &&
+strcmp(argv[2], "%"))
+)
+{
+printf("Error\n");
+exit(99);
+}
 
-	printf("%d\n", operation(i, j));
-	return (0);
+if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0)
+&& atoi(argv[3]) == 0)
+{
+printf("Error\n");
+exit(100);
+}
+
+printf("%d\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
+
+return (0);
 }
